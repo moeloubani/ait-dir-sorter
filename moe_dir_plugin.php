73b260b47244575bc1f0e515ad43e2a3f5db3bf7
moeloubani <moe@loubani.com>
2013-04-26 05:44:46 -0300

    Added file

---
+++ moe_dir_plugin.php
@@ -0,0 +1,32 @@
+<?php 
+/*
+Plugin Name: AIT Directory Sorter
+Plugin URI: http://moeloubani.com/ait-directory-sorting-plugin
+Description: A plugin that adds a sort box to allow alphabetical sorting in searches.
+Version: 1.0
+Author: Moe Loubani
+Author URI: http://moeloubani.com
+*/
+
+function moe_add_js() {
+  wp_enqueue_script(
+		'moe_plug_js',
+		plugins_url( '/js/script.js' , __FILE__ ),
+		array( 'jquery' )
+	);
+}
+
+add_action( 'wp_enqueue_scripts', 'moe_add_js' );
+
+function moe_pre_posts($query) {
+	if($query->is_main_query() && !$query->is_admin) {
+		if($_GET['ml_order'] == 'alpha') {
+				$query->set('orderby','title');
+				$query->set('order', 'ASC');
+			}
+		else {
+				$query->set('orderby', 'date');
+			}
+		}
+	}
+add_filter('pre_get_posts', 'moe_pre_posts', 100);
